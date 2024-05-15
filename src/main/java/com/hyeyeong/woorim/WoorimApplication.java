28ada6f22ac0c1c0eb80ef09dc764fae221901e1
dhk5646@gmail.com <>
2024-05-15 18:18:48 +0900

    initialize

---
+++ src/main/java/com/hyeyeong/woorim/WoorimApplication.java
@@ -0,0 +1,13 @@
+package com.hyeyeong.woorim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoorimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoorimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyeyeong/woorim/WoorimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeyeong.woorim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoorimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
