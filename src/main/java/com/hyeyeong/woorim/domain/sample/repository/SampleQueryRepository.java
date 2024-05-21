c09b9c9e7d3a4e8dcf4c96e1f006e2553adcc1ad
dhk5646@gmail.com <>
2024-05-21 23:07:22 +0900

    feat. querydsl settings

--- src/main/java/com/hyeyeong/woorim/domain/sample/repository/SampleQueryRepository.java
+++ src/main/java/com/hyeyeong/woorim/domain/sample/repository/SampleQueryRepository.java
@@ -1,9 +1,13 @@
 package com.hyeyeong.woorim.domain.sample.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class SampleQueryRepository {
-    
+
+    private final JPAQueryFactory jpaQueryFactory;
 
 }
